c81bfaec779a3093bcf267279c47e1c843c84d11
ZetChaTori <altair_altair@outlook.fr>
2025-06-28 15:35:49 +0200

    do something of the scheduler

---
+++ src/kernel/scheduler/autogroup/autogroup.rs
@@ -0,0 +1,15 @@
+// File for the autogroup scheduler module.
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImpl;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTrait;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV1;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV2;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV3;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV4;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV5;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV6;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV7;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV8;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV9;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV10;
+use crate::kernel::scheduler::autogroup::autogroup_impl::AutogroupImplTraitV11;
+
